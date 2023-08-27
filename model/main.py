543718f38a71b8df198b15329d0d802d7b42d92f
taniacsilva <106198492+taniacsilva@users.noreply.github.com>
2023-08-27 23:27:52 +0100

    Include utils module

--- model/main.py
+++ model/main.py
@@ -1,6 +1,7 @@
 """This is the main module of this exercise"""
 from load_data import extract_zip
 from cleaning import clean, topn
+from utils import split_data
 
 url = 'https://github.com/mattharrison/datasets/raw/master/data/kaggle-survey-2018.zip'
 filename = 'kaggle-survey-2018.zip'
@@ -12,9 +13,8 @@ def main():
     """
     raw = extract_zip(url, filename, member_name)
 
-    cleaned_data = clean(df_ = raw)
+    X_train = split_data(raw = raw, ycol = 'Q6')
 
-    top_n = topn(ser = cleaned_data)
-
+    print(X_train)
 if __name__ == '__main__':
     main()
--- model/utils.py
+++ model/utils.py
@@ -2,11 +2,14 @@ from sklearn import model_selection
 from transformer import get_rawX_y, Transformer
 from feature_engine import encoding, imputation
 from sklearn import pipeline
+from sklearn.preprocessing import StandardScaler
 
-def pipeline():
+def do_pipeline():
     ## Create a pipeline
     kag_pl = pipeline.Pipeline(
         [('clean', Transformer()),
+        # ('preprocess_num', StandardScaler(), ['numeric_col_1', 'numeric_col_2']),  # Preprocess non-categorical columns
+
         ('cat', encoding.OneHotEncoder(top_categories = 5,
                                     drop_last = True,
                                     variables=['Q1', 'Q3', 'major']
@@ -18,12 +21,15 @@ def pipeline():
 
 def split_data (raw, ycol): 
     set_X, set_y = get_rawX_y(df=raw, y_col=ycol)
+    kag_pl = do_pipeline()
     set_train_X, set_test_X, set_train_y, set_test_y = model_selection.train_test_split(
         set_X,
         set_y,
         test_size=0.3,
         random_state=42,
-        stratify=set_y
-    )
+        stratify=set_y)
 
-    kag_pl = pipeline()
+    X_train = kag_pl.fit_transform(set_train_X, set_train_y)
+    X_test = kag_pl.transform(set_test_X)
+
+    return X_train
